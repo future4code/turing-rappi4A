00fa73147c5c88a880c4ef1cebee47e8989a67d9
leogsousa <leonardogomessousa5@gmail.com>
2020-08-10 19:01:44 -0300

    Feita a tela de Login, estilo e funcionalidades, falta corrigit as rotas

---
+++ src/TelaLogin/styles.js
@@ -0,0 +1,190 @@
+import styled from 'styled-components'  
+
+export const Login = styled.div`
+  font-family: Roboto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  width: 360px;
+  height: 640px;
+`
+export const LogoIcon = styled.img`
+  width: 104px;
+  height: 58px;
+  object-fit: contain;
+  position: fixed;
+  left: 128px;
+  top: 88px;
+`
+export const TitleEnter = styled.div`
+  width: 360px;
+  height: 42px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  top: 162px;
+`
+export const Title = styled.div`
+  width: 360px;
+  height: 42px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  top: 414px;
+`
+export const Text = styled.p`
+  width: 296px;
+  height: 18px;
+  font-size: 16px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: -0.39px;
+  text-align: center;
+  color: var(--black);
+`
+export const FormLogin = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+export const InputNolocusEmail = styled.div`
+  width: 360px;
+  height: 72px;
+  position: fixed;
+  top: 204px;
+`
+export const InputNolocusPassword = styled.div`
+  width: 360px;
+  height: 72px;
+  position: fixed;
+  top: 276px;
+`
+export const RectangleInputEmail = styled.div`
+  width: 328px;
+  height: 56px;
+  border-radius: 2px;
+  border: solid 1px #b8b8b8;
+  position: fixed;
+  left: 16px;
+  top: 212px;
+`
+export const RectangleInputPassword = styled.div`
+  width: 328px;
+  height: 56px;
+  border-radius: 2px;
+  border: solid 1px #b8b8b8;
+  position: fixed;
+  left: 16px;
+  top: 284px;
+`
+export const ViewPasswordIcon = styled.img`
+  width: 24px;
+  height: 24px;
+  object-fit: contain;
+  position: fixed;
+  left: 304px;
+  top: 300px;
+`
+export const LabelInput = styled.p`
+  width: 78px;
+  height: 18px;
+  font-family: Roboto;
+  font-size: 12px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: -0.29px;
+  color: #b8b8b8;
+  margin: 0 0 0 4px;
+`
+export const RectangleLabelEmail = styled.div`
+  width: 80px;
+  height: 20px;
+  background-color: var(--white);
+  position: fixed;
+  left: 28px;
+  top: 204px;
+  background-color: #FFF;
+`
+export const RectangleLabelPassword = styled.div`
+  width: 80px;
+  height: 20px;
+  background-color: var(--white);
+  position: fixed;
+  left: 28px;
+  top: 276px;
+  background-color: #FFF;
+`
+export const FormInputEmail = styled.input`
+  width: 264px;
+  height: 18px;
+  font-family: Roboto;
+  font-size: 16px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: -0.39px;
+  color: #d0d0d0;
+  border: none;
+  outline: none;
+  position: fixed;
+  left: 32px;
+  top: 231px;
+`
+export const FormInputPassword = styled.input`
+  width: 264px;
+  height: 18px;
+  font-family: Roboto;
+  font-size: 16px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: -0.39px;
+  color: #d0d0d0;
+  border: none;
+  outline: none;
+  position: fixed;
+  left: 32px;
+  top: 303px;
+`
+export const ButtonEnter = styled.button`
+  width: 296px;
+  height: 18px;
+  font-size: 16px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: -0.39px;
+  text-align: center;
+  color: var(--black);
+  background-color: #e86e5a;
+  border: none;
+  position: fixed;
+  left: 32px;
+  top: 368px;
+`
+export const RectangleButton = styled.div`
+  width: 328px;
+  height: 42px;
+  border-radius: 2px;
+  background-color: #e86e5a;
+  position: fixed;
+  left: 16px;
+  top: 356px;
+`
+export const ButtonNolocus = styled.div`
+  width: 360px;
+  height: 42px;
+  position: fixed;
+  top: 356px;
+`
