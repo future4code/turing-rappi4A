8fa08725dd58df2659e5efa6f216120786042007
Vinícius Fredeanelle <fredaummmassis@gmail.com>
2020-08-11 17:53:42 -0300

    Tela cadastro estilizada e com funcionalidades prontas

--- src/TelaDeCadastro/styles.js
+++ src/TelaDeCadastro/styles.js
@@ -1,5 +1,65 @@
 import styled from "styled-components";
 
 export const CamposDeCadastro = styled.input`
-border: ${props => props.borda};
+    border: ${props => props.borda};
+    width: 92vw;
+    height: 56px;
+    border-radius: 4px;
+    margin-bottom: 16px;
+    padding-left: 8px;
+`
+export const Imagem = styled.img`
+    position: absolute;
+    right: 16px;
+    top: 16px;
+`
+export const Form = styled.form`
+    padding: 16px;
+    padding-top: 24px;
+
+    div {
+        position: relative;
+    }
+
+    label {
+        position: absolute;
+        left: 16px;
+        top: -8px;
+        background-color: #fff;
+        display: block;
+        padding: 0 2px;
+        width: 78px;
+        height: 18px;
+        font-family: Roboto;
+        font-size: 12px;
+        font-weight: normal;
+        font-stretch: normal;
+        font-style: normal;
+        line-height: normal;
+        letter-spacing: -0.29px;
+        color: #b8b8b8;
+
+    }
+
+    input {
+        width: 89vw;
+        height: 56px;
+        border-radius: 4px;
+        margin-bottom: 16px;
+        padding-left: 8px;
+    }
+
+    button {
+        width: 92vw;
+        height: 42px;
+        border-radius: 2px;
+        background-color: #e86e5a;
+        font-weight: normal;
+        font-stretch: normal;
+        font-style: normal;
+        line-height: normal;
+        letter-spacing: -0.39px;
+        text-align: center;
+        border: none;
+    }
 `
