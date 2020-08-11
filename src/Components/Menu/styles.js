f525a8147bcc3797c59dc5566967e049ecdd75c1
Dienay Lima <dienaylima@gmail.com>
2020-08-11 16:11:16 -0300

    Componente Menu

---
+++ src/Components/Menu/styles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+export const ContainerMenu = styled.div`
+    border-top: 1px solid rgba(0, 0, 0, 0.25);
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    height: 49px;
+    justify-content: center;
+    position: absolute;
+    bottom: 0;
+    width: 100vw;
+
+    div {
+        align-items: center;
+        display: flex;
+        justify-content: center;
+        width: 100%;
+    }
+
+    img {
+        width: 27px;
+    }
+`
